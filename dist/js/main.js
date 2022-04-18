fbed5e96298371b8d68b378ec1bf20c7ea229ebd
Maglen-gg <bryan.with.a.wi@gmail.com>
2022-04-17 20:20:35 -0400

    Created dad jokes

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+/* eslint-disable prefer-destructuring */
+const getRandomDadJoke = async () => {
+  const url = '/.netlify/functions/jokes';
+  const jokeStream = await fetch(url);
+  const jsonJoke = await jokeStream.json();
+  const joke = jsonJoke.joke;
+  return joke;
+};
+
+const displayJoke = (joke) => {
+  const h1 = document.querySelector('h1');
+  h1.textContent = joke;
+};
+
+const refreshJoke = async () => {
+  const joke = await getRandomDadJoke();
+  displayJoke(joke);
+};
+
+refreshJoke();
+
+setInterval(refreshJoke, 3000);
---
+++ serverless/jokes.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+/* eslint-disable implicit-arrow-linebreak */
+// mod.cjs
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+exports.handler = async (event, context) => {
+  const url = 'https://icanhazdadjoke.com/';
+  try {
+    const jokeStream = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    const jsonJoke = await jokeStream.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jsonJoke),
+    };
+  } catch (err) {
+    return { statusCode: 422, body: err.stack };
+  }
+};
